17e893e7719837f5b1785b60468b63afbeaa7dd4
Steff Kempink <mswf@live.nl>
2016-07-07 16:34:24 +0200

    + It's a GraviSword!

--- ZwaardeKracht_Project/Assets/Scripts/Gameplay/Controller/GraviSword.cs
+++ ZwaardeKracht_Project/Assets/Scripts/Gameplay/Controller/GraviSword.cs
@@ -1,15 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityStandardAssets.CrossPlatformInput;
 
-public class GraviSword : MonoBehaviour {
+public class GraviSword : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
+	public Transform swordPivot;
+
+	public float currentRotation = 0f;
+	public float rotateSensitivity = 1f;
+
+	public void Start()
+	{
+		currentRotation = 180f;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void UpdateInput(RigidbodyFirstPersonController controller, Vector2 input)
+	{
+		var rotateInput = CrossPlatformInputManager.GetAxis("Mouse Y")*rotateSensitivity;
+
+		currentRotation += rotateInput;
+
+		swordPivot.localRotation = Quaternion.Euler(0, 0, currentRotation);
+
+		var newGravity = swordPivot.up * 9.81f;
+
+		DebugExtension.DebugArrow(Vector3.zero, newGravity, Color.yellow, Time.fixedDeltaTime, false);
+
+		Physics.gravity = newGravity;
 	}
+
 }
