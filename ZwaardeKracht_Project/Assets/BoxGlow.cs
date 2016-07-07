fbe4d53e52a92b826a1e25df343de93e37a457ef
Steff Kempink <mswf@live.nl>
2016-07-07 17:09:28 +0200

    + All the effects

---
+++ ZwaardeKracht_Project/Assets/BoxGlow.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxGlow : MonoBehaviour
+{
+
+	private Rigidbody rigidBody;
+	private Light light;
+	private float previousIntensity;
+
+	private Material material;
+
+	protected void Awake()
+	{
+		rigidBody = GetComponent<Rigidbody>();
+		light = GetComponent<Light>();
+
+		material = GetComponent<MeshRenderer>().material;
+	}
+
+	void FixedUpdate()
+	{
+		float intensity = 0f;
+
+		intensity += rigidBody.angularVelocity.magnitude;
+		intensity += rigidBody.velocity.magnitude;
+
+		intensity = Mathf.Max(previousIntensity, intensity);
+
+		light.intensity = intensity * 0.2f;
+
+		var color = light.color;
+
+		float H, S, V;
+
+		Color.RGBToHSV(light.color, out H, out S, out V);
+		V = intensity*0.04f;
+
+		material.SetColor("_EmissionColor", Color.HSVToRGB(H, S, V));
+
+		previousIntensity = intensity*0.9f;
+
+	}
+	
+}
